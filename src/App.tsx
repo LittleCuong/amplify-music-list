162a7ef9fb347a4acaa8964bae33953f2e0b1d8b
Cuong Nguyen <littlecuong922@gmail.com>
2024-05-10 10:31:18 +0700

    Add auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from 'react'
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "../amplify/data/resource";
+import { Authenticator } from '@aws-amplify/ui-react';
 import './App.css'
+import '@aws-amplify/ui-react/styles.css';
 import MusicItem from './components/MusicItem';
 
 const client = generateClient<Schema>()
@@ -34,30 +36,35 @@ function App() {
   }
 
   return (
-    <>
-      <div className='container'>
-        <div className="card card-form">
-          <form className='music-form'>
-            <input className='music-form--input' name="name" onChange={(e) => handleOnChange(e)} placeholder='Name music'/>
-            <input className='music-form--input' name="author" onChange={(e) => handleOnChange(e)} placeholder='Author'/>
-            <input className='music-form--input' name="album" onChange={(e) => handleOnChange(e)} placeholder='Album'/>
-          </form>
-          <button className='add-button' onClick={addMusic}>
-            Add music
-          </button>
-        </div>
-        <div className="card">
-            <h3 style={{margin: 0, fontSize: '20px'}}>My music</h3>
-            <ul className="music-list">
-            {
-              favouriteMusic.map((music) => (
-                <MusicItem key={music.id} songData={music}/>
-              ))
-            }
-            </ul>
-          </div>
-      </div>
-    </>
+    <Authenticator>
+      {({ signOut, user }) => (
+          <>
+            <h3>Hello {user?.username}</h3>
+            <div className='container'>
+              <div className="card card-form">
+                <form className='music-form'>
+                  <input className='music-form--input' name="name" onChange={(e) => handleOnChange(e)} placeholder='Name music'/>
+                  <input className='music-form--input' name="author" onChange={(e) => handleOnChange(e)} placeholder='Author'/>
+                  <input className='music-form--input' name="album" onChange={(e) => handleOnChange(e)} placeholder='Album'/>
+                </form>
+                <button className='add-button' onClick={addMusic}>
+                  Add music
+                </button>
+              </div>
+              <div className="card">
+                  <h3 style={{margin: 0, fontSize: '20px'}}>My music</h3>
+                  <ul className="music-list">
+                  {
+                    favouriteMusic.map((music) => (
+                      <MusicItem key={music.id} songData={music}/>
+                    ))
+                  }
+                  </ul>
+                </div>
+            </div>
+          </>
+      )}
+    </Authenticator>
   )
 }
 
